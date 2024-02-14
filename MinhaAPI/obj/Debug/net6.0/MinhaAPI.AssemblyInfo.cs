0e9fb5162436e42d32c77cd87fe059db935c3df9
Diego Santos Rodrigues <diego.rodrigues@norber.com.br>
2024-02-14 10:41:11 -0300

    alterando caminho

--- MinhaAPI/obj/Debug/net6.0/MinhaAPI.AssemblyInfo.cs
+++ MinhaAPI/obj/Debug/net6.0/MinhaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130e9575827c63d277bf74ff1577572099964103")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinhaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
